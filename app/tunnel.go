b081b8284856e8fb8d0001498d31533bf4405d4c
Andrei Volnin <andrei.volnin@frozy.io>
2018-12-12 13:26:43 +0700

    Setup single ssh connection for consumer

--- app/tunnel.go
+++ app/tunnel.go
@@ -17,10 +17,14 @@ func runConsumer(conf *Config) error {
 		return err
 	}
 
-	fmt.Printf("Listening at %s to consume resource %s (brocker at %s)\n",
-		conf.Connector.Addr.String(), conf.remoteResourse().String(), conf.Init.Broker.String())
-
 	for {
+		serverConn, err := ssh.Dial("tcp", conf.Init.Broker.String(), sshConfig)
+		if err != nil {
+			fmt.Printf("SSH server dial error: %s\n", err)
+			return err
+		}
+		defer serverConn.Close()
+
 		listener, err := net.Listen("tcp", conf.Connector.Addr.String())
 		if err != nil {
 			fmt.Printf("Local listen error %s\n", err.Error())
@@ -28,45 +32,31 @@ func runConsumer(conf *Config) error {
 		}
 		defer listener.Close()
 
+		fmt.Printf("Listening at %s to consume resource %s (brocker at %s)\n",
+			conf.Connector.Addr.String(), conf.remoteResourse().String(), conf.Init.Broker.String())
+
 		for {
 			conn, err := listener.Accept()
 			if err != nil {
 				fmt.Printf("Local accept error %s\n", err.Error())
 				break
 			}
-			go conf.consumerForward(conn, sshConfig)
+			go conf.consumerForward(conn, serverConn)
 		}
 
 		time.Sleep(pollInterval)
 	}
 }
 
-func (conf Config) consumerForward(localConn net.Conn, sshConfig *ssh.ClientConfig) {
-	serverConn, err := ssh.Dial("tcp", conf.Init.Broker.String(), sshConfig)
-	if err != nil {
-		fmt.Printf("SSH server dial error: %s\n", err)
-		return
-	}
-
-	remoteConn, err := serverConn.Dial("tcp", conf.remoteResourse().String())
+func (conf Config) consumerForward(localConn net.Conn, sshClient *ssh.Client) {
+	remoteConn, err := sshClient.Dial("tcp", conf.remoteResourse().String())
 	if err != nil {
 		fmt.Printf("SSH remote dial error: %s\n", err)
 		return
 	}
 	defer remoteConn.Close()
 
-	done := make(chan bool)
-	copyConn := func(writer, reader net.Conn) {
-		_, err := io.Copy(writer, reader)
-		if err != nil {
-			fmt.Printf("io.Copy error: %s\n", err)
-		}
-		done <- true
-	}
-
-	go copyConn(localConn, remoteConn)
-	go copyConn(remoteConn, localConn)
-	<-done
+	connectionForward(remoteConn, localConn)
 }
 
 func runProvider(conf *Config) error {
@@ -75,15 +65,16 @@ func runProvider(conf *Config) error {
 		return err
 	}
 
-	fmt.Printf("Providing resource %s from %s (brocker at %s)\n",
-		conf.remoteResourse().String(), conf.Connector.Addr.String(), conf.Init.Broker.String())
-
 	for {
 		listener, err := listener(conf.remoteResourse(), conf.Init.Broker, sshConfig)
 		if err != nil {
-			return fmt.Errorf("Remote SSH listen error: %s", err.Error())
+			fmt.Printf("Remote SSH listen error: %s\n", err.Error())
+			return err
 		}
 		defer listener.Close()
+
+		fmt.Printf("Providing resource %s from %s (brocker at %s)\n",
+			conf.remoteResourse().String(), conf.Connector.Addr.String(), conf.Init.Broker.String())
 
 		for {
 			conn, err := listener.Accept()
@@ -91,25 +82,30 @@ func runProvider(conf *Config) error {
 				fmt.Printf("Remote accept error: %s\n", err.Error())
 				break
 			}
-
-			target, err := net.Dial("tcp", conf.Connector.Addr.String())
-			if err != nil {
-				fmt.Printf("Dial into target service error: %s\n", err.Error())
-			}
-
-			go conf.providerForward(conn, target)
+			defer conn.Close()
+			go conf.providerForward(conn)
 		}
 
 		time.Sleep(pollInterval)
 	}
 }
 
-func (conf Config) providerForward(remoteConn net.Conn, targetConn net.Conn) {
+func (conf Config) providerForward(conn net.Conn) {
+	target, err := net.Dial("tcp", conf.Connector.Addr.String())
+	if err != nil {
+		fmt.Printf("Dial into target service error: %s\n", err.Error())
+		return
+	}
+	defer target.Close()
+	connectionForward(conn, target)
+}
+
+func connectionForward(remoteConn net.Conn, targetConn net.Conn) {
 	done := make(chan bool)
 	copyConn := func(writer io.Writer, reader io.Reader) {
 		_, err := io.Copy(writer, reader)
 		if err != nil {
-			fmt.Printf("io.Copy error: %s\n", err.Error())
+			fmt.Printf("Connection forwarding finished with: %s\n", err.Error())
 		}
 		done <- true
 	}
